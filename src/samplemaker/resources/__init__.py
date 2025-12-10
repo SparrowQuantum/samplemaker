9a41cb574208acf5dd1e76337c5d4af321fd3efd
JeppeSparrow <115155927+JeppeSparrow@users.noreply.github.com>
2025-12-10 09:48:39 +0100

    Feat: Add boopy stub file

--- src/samplemaker/resources/__init__.py
+++ src/samplemaker/resources/__init__.py
@@ -5,13 +5,3 @@ Resources module for samplemaker.
 This module provides access to resource files and compiled extensions,
 including the boopy C++ extension for Boolean polygon operations.
 """
-
-# Import the boopy C++ extension
-try:
-    from . import boopy
-
-    __all__ = ["boopy"]
-except ImportError as e:
-    msg = "Failed to import boopy module. "
-    "Make sure the C++ extension is compiled correctly."
-    raise ImportError(msg) from e
