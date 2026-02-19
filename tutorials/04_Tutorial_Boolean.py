f8f0050700ff6cbd08b4bb1b68bfe8466657004a
JeppeSparrow <jeppe.roulund@sparrowquantum.com>
2026-02-19 11:13:58 +0100

    Chore: Format tutorials

--- tutorials/04_Tutorial_Boolean.py
+++ tutorials/04_Tutorial_Boolean.py
@@ -3,65 +3,67 @@
 """
 
 
-# In this tutorial we learn how to do boolean operations between groups of 
-# polygons
+# In this tutorial we learn how to do boolean operations between groups of polygons.
 
 # Let's import basic stuff
-import samplemaker.layout as smlay # used for layout 
-import samplemaker.makers as sm # used for drawing
+import samplemaker.layout as smlay  # used for layout
+import samplemaker.makers as sm  # used for drawing
 
 # Create a simple mask layout
 themask = smlay.Mask("04_Tutorial_Boolean")
 
 # Empty geometry
-geomE = sm.GeomGroup()
+g = sm.GeomGroup()
 
 # Let's make a large box
-box0 = sm.make_rect(0,0,100,100,layer=1)
+box0 = sm.make_rect(0, 0, 100, 100, layer=1)
 
 # And some text, because text is complex polygons!
-text0 = sm.make_text(0, 0, "DIFF", 10, 2,angle=30,to_poly=True,layer=1)
+text0 = sm.make_text(0, 0, "DIFF", 10, 2, angle=30, to_poly=True, layer=1)
 
-# Let's take the boolean difference box-text
-bdiff = box0.copy() # Note that boolean operations alter the original element so we need to make a copy first
+# Let's take the boolean difference box-text.
+# Boolean operations alter the original element so we need to make a copy first:
+bdiff = box0.copy()
 bdiff.boolean_difference(text0, 1, 1)
-# The first integer is the layer from which you should subtract and the second is the subtracted layer
-# Now bdiff is box-text
-geomE+=bdiff
+# The first integer is the layer from which you should subtract and the second is the
+# subtracted layer.
 
-# Now let's try intersection (AND operation)
+# Now bdiff is box-text
+g += bdiff
+
+# Now let's try intersection (AND operation):
 # Let's use two overlapping texts, slighlty larger
-text1 = sm.make_text(0,0,"DIFF",11,3,angle=30,to_poly=True,layer=1)
+text1 = sm.make_text(0, 0, "DIFF", 11, 3, angle=30, to_poly=True, layer=1)
 text1.boolean_intersection(text0, 1, 1)
 text1.translate(100, 0)
-geomE+=text1
+g += text1
 
 # XOR is also quite useful, only keeps parts that are not in both
-text2 = sm.make_text(50,0,"XOR",10,1,angle=0,to_poly=True,layer=1)
+text2 = sm.make_text(50, 0, "XOR", 10, 1, angle=0, to_poly=True, layer=1)
 text2.boolean_xor(box0, 1, 1)
 text2.translate(200, 0)
-geomE+=text2
+g += text2
 
 # Trapezoid slicing, useful for some e-beam export
 trapz = text2.copy()
 trapz.trapezoids(1)
 trapz.translate(150, 0)
-geomE+=trapz
+g += trapz
 
 # Union, we could re-unite all trapezoids in the previous
 uni1 = trapz.copy()
 uni1.boolean_union(1)
 uni1.translate(150, 0)
-geomE+=uni1
+g += uni1
 
 # Just for fun, outlining the last result
 out1 = uni1.copy()
 out1.poly_outlining(1, 1)
 out1.translate(150, 0)
-geomE+=out1
+g += out1
 
 # Let's add all to main cell
-themask.addToMainCell(geomE)    
+themask.addToMainCell(g)
 
 # Export to GDS
 themask.exportGDS()
