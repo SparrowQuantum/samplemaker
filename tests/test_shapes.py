3aeb9a62eed9451db6c43dfc206212d2aca2e41d
JeppeSparrow <jeppe.roulund@sparrowquantum.com>
2026-02-24 16:14:01 +0100

    Feat: Path tests

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -6,7 +6,7 @@ import pytest
 from samplemaker import shapes as sp
 
 _COORD = tuple[float, float]
-_LF = list[float]
+_TF = tuple[float, ...]
 
 
 @pytest.fixture
@@ -29,17 +29,31 @@ def box(box_llxy: _COORD, box_wh: _COORD) -> sp.Box:
 
 
 @pytest.fixture
-def poly_pts() -> tuple[_LF, _LF]:
+def poly_pts() -> tuple[_TF, _TF]:
     """Points in test polygon."""
-    xpts = [0.0, 3.0, 3.0, 0.0]
-    ypts = [0.0, 0.0, 2.0, 2.0]
+    xpts = (0.0, 3.0, 3.0, 0.0)
+    ypts = (0.0, 0.0, 2.0, 2.0)
     return xpts, ypts
 
 
 @pytest.fixture
-def poly(poly_pts: tuple[_LF, _LF]) -> sp.Poly:
+def poly(poly_pts: tuple[_TF, _TF]) -> sp.Poly:
     xpts, ypts = poly_pts
     return sp.Poly(xpts, ypts, layer=2)
+
+
+@pytest.fixture
+def path_width() -> float:
+    """Width of test path."""
+    return 0.5
+
+
+@pytest.fixture
+def path(poly_pts: tuple[_TF, _TF], path_width: float) -> sp.Path:
+    xpts, ypts = poly_pts
+    # xpts and ypts need to be mutable for the transformations,
+    # so we convert them to lists here.
+    return sp.Path(list(xpts), list(ypts), path_width, layer=2)
 
 
 def test_dot_transformations() -> None:
@@ -149,7 +163,7 @@ class TestPoly:
 
     """
 
-    def test_init_poly(self, poly: sp.Poly, poly_pts: tuple[_LF, _LF]) -> None:
+    def test_init_poly(self, poly: sp.Poly, poly_pts: tuple[_TF, _TF]) -> None:
         # poly.data should be of the format
         # [x0, y0, x1, y1, x2, y2, x3, y3, ..., x0, y0]
         assert isinstance(poly.data, np.ndarray)
@@ -160,12 +174,12 @@ class TestPoly:
         reshaped_data = poly.data.reshape((-1, 2))
         resh_xpts, resh_ypts = reshaped_data[:, 0], reshaped_data[:, 1]
         # Close the polygon by repeating the first point at the end:
-        expected_xpts = poly_pts[0] + [poly_pts[0][0]]
-        expected_ypts = poly_pts[1] + [poly_pts[1][0]]
+        expected_xpts = poly_pts[0] + (poly_pts[0][0],)
+        expected_ypts = poly_pts[1] + (poly_pts[1][0],)
         assert list(resh_xpts) == pytest.approx(expected_xpts)
         assert list(resh_ypts) == pytest.approx(expected_ypts)
 
-    def test_bounding_box(self, poly: sp.Poly, poly_pts: tuple[_LF, _LF]) -> None:
+    def test_bounding_box(self, poly: sp.Poly, poly_pts: tuple[_TF, _TF]) -> None:
         bb = poly.bounding_box()
         xpts, ypts = poly_pts
         expected_llx = min(xpts)
@@ -251,9 +265,9 @@ class TestPoly:
         assert poly.area() == pytest.approx(6.0)
         assert poly.perimeter() == pytest.approx(10.0)
 
-    def test_int_data(self, poly_pts: tuple[_LF, _LF]) -> None:
-        expected_xpts = poly_pts[0] + [poly_pts[0][0]]
-        expected_ypts = poly_pts[1] + [poly_pts[1][0]]
+    def test_int_data(self, poly_pts: tuple[_TF, _TF]) -> None:
+        expected_xpts = poly_pts[0] + (poly_pts[0][0],)
+        expected_ypts = poly_pts[1] + (poly_pts[1][0],)
         scaled_xpts = [1000 * x for x in expected_xpts]
         scaled_ypts = [1000 * y for y in expected_ypts]
         int_data = np.array([scaled_xpts, scaled_ypts], dtype=int).T.reshape(-1)
@@ -314,3 +328,182 @@ class TestPoly:
         assert poly.point_inside(4.0, 1.0) is False
         assert poly.point_inside(1.5, -1.0) is False
         assert poly.point_inside(1.5, 3.0) is False
+
+
+class TestPath:
+    def test_init_path(
+        self, path: sp.Path, poly_pts: tuple[_TF, _TF], path_width: float
+    ) -> None:
+        xpts, ypts = poly_pts
+        assert path.xpts == list(xpts)
+        assert path.ypts == list(ypts)
+        assert path.width == path_width
+        assert path.layer == 2
+        assert path.Npts == len(xpts)
+
+    def test_bounding_box(self, path: sp.Path, poly_pts: tuple[_TF, _TF]) -> None:
+        bb = path.bounding_box()
+        xpts, ypts = poly_pts
+        expected_llx = min(xpts)
+        expected_lly = min(ypts)
+        expected_urx = max(xpts)
+        expected_ury = max(ypts)
+        expected_width = expected_urx - expected_llx
+        expected_height = expected_ury - expected_lly
+
+        assert isinstance(bb, sp.Box)
+        assert bb.llx == pytest.approx(expected_llx)
+        assert bb.lly == pytest.approx(expected_lly)
+        assert bb.urx() == pytest.approx(expected_urx)
+        assert bb.ury() == pytest.approx(expected_ury)
+        assert bb.width == pytest.approx(expected_width)
+        assert bb.height == pytest.approx(expected_height)
+
+    def test_translate(self, path: sp.Path, poly_pts: tuple[_TF, _TF]) -> None:
+        xpts, ypts = poly_pts
+        path.translate(1.0, 2.0)
+        expected_xpts = [x + 1.0 for x in xpts]
+        expected_ypts = [y + 2.0 for y in ypts]
+        assert path.xpts == pytest.approx(expected_xpts)
+        assert path.ypts == pytest.approx(expected_ypts)
+
+    def test_rotate(self, path: sp.Path, poly_pts: tuple[_TF, _TF]) -> None:
+        xpts, ypts = poly_pts
+        path.rotate(0.0, 0.0, 90.0)
+        expected_xpts = [-y for y in ypts]
+        expected_ypts = [x for x in xpts]
+        assert path.xpts == pytest.approx(expected_xpts)
+        assert path.ypts == pytest.approx(expected_ypts)
+
+    def test_rotate_translate(self, path: sp.Path, poly_pts: tuple[_TF, _TF]) -> None:
+        xpts, ypts = poly_pts
+        path.rotate_translate(2.0, 3.0, 180.0)
+        expected_xpts = [2.0 - x for x in xpts]
+        expected_ypts = [3.0 - y for y in ypts]
+        assert path.xpts == pytest.approx(expected_xpts)
+        assert path.ypts == pytest.approx(expected_ypts)
+
+    def test_scale(self, path: sp.Path, poly_pts: tuple[_TF, _TF]) -> None:
+        xpts, ypts = poly_pts
+        path.scale(0.0, 0.0, 2.0, 3.0)
+        expected_xpts = [2.0 * x for x in xpts]
+        expected_ypts = [3.0 * y for y in ypts]
+        assert path.xpts == pytest.approx(expected_xpts)
+        assert path.ypts == pytest.approx(expected_ypts)
+
+    def test_mirror_x(self, path: sp.Path, poly_pts: tuple[_TF, _TF]) -> None:
+        xpts, ypts = poly_pts
+        path.mirrorX(1.5)
+        expected_xpts = [2 * 1.5 - x for x in xpts]
+        expected_ypts = list(ypts)
+        assert path.xpts == pytest.approx(expected_xpts)
+        assert path.ypts == pytest.approx(expected_ypts)
+
+    def test_mirror_y(self, path: sp.Path, poly_pts: tuple[_TF, _TF]) -> None:
+        xpts, ypts = poly_pts
+        path.mirrorY(1.0)
+        expected_xpts = list(xpts)
+        expected_ypts = [2 * 1.0 - y for y in ypts]
+        assert path.xpts == pytest.approx(expected_xpts)
+        assert path.ypts == pytest.approx(expected_ypts)
+
+    def test_path_length(self, path: sp.Path) -> None:
+        # Two sides of length 3 and one side of length 2 -> total length 8
+        assert path.path_length() == pytest.approx(8.0)
+
+    def test_area(self, path: sp.Path) -> None:
+        # The area of a path is defined as the area of the polygon formed by
+        # the centerline of the path and the width. For a rectangle with width
+        # 0.5 and length 8, the area should be 4.
+        assert path.area() == pytest.approx(4.0)
+
+    def test_perimeter(self, path: sp.Path) -> None:
+        # Outside perimeter of the path is approximately twice the path length
+        # plus twice the path width. For a path length of 8 and width of 0.5,
+        # the perimeter should be approximately 17.0.
+        assert path.perimeter() == pytest.approx(17.0)
+
+    def test_to_polygon_single_points(self) -> None:
+        # A path with only one point converts to a square cap polygon with side length
+        # equal to the path width.
+        path = sp.Path([0.0], [0.0], width=1.0, layer=2)
+        g = path.to_polygon()
+
+        # Square cap + closing point -> 5 points in total
+        expected_xpts = [-0.5, 0.5, 0.5, -0.5, -0.5]
+        expected_ypts = [-0.5, -0.5, 0.5, 0.5, -0.5]
+
+        assert isinstance(g, sp.GeomGroup)
+        assert len(g.group) == 1
+        poly = g.group[0]
+        assert isinstance(poly, sp.Poly)
+        xpts = poly.data[0::2]
+        ypts = poly.data[1::2]
+
+        assert poly.layer == 2
+        assert xpts == pytest.approx(expected_xpts)
+        assert ypts == pytest.approx(expected_ypts)
+
+    def test_to_polygon_line_segment(self) -> None:
+        # A path with two points converts to a rectangle with length equal to the
+        # distance between the two points and width equal to the path width.
+        path = sp.Path([0.0, 3.0], [0.0, 0.0], width=1.0, layer=2)
+        g = path.to_polygon()
+
+        expected_xpts = [0.0, 3.0, 3.0, 0.0, 0.0]
+        expected_ypts = [-0.5, -0.5, 0.5, 0.5, -0.5]
+
+        assert isinstance(g, sp.GeomGroup)
+        assert len(g.group) == 1
+        poly = g.group[0]
+        assert isinstance(poly, sp.Poly)
+        xpts = poly.data[0::2]
+        ypts = poly.data[1::2]
+
+        assert poly.layer == 2
+        assert xpts == pytest.approx(expected_xpts)
+        assert ypts == pytest.approx(expected_ypts)
+
+    def test_to_polygon_polyline(self, path: sp.Path) -> None:
+        # A multi-segment path converts to a single polygon outlining the stroke.
+        g = path.to_polygon()
+
+        half = path.width / 2
+        expected_xpts = [
+            0.0,
+            3.0,
+            3.0 + half,
+            3.0 + half,
+            3.0,
+            0.0,
+            0.0,
+            3.0 - half,
+            3.0 - half,
+            0.0,
+            0.0,
+        ]
+        expected_ypts = [
+            -half,
+            -half,
+            0.0,
+            2.0,
+            2.0 + half,
+            2.0 + half,
+            2.0 - half,
+            2.0 - half,
+            half,
+            half,
+            -half,
+        ]
+
+        assert isinstance(g, sp.GeomGroup)
+        assert len(g.group) == 1
+        poly = g.group[0]
+        assert isinstance(poly, sp.Poly)
+        xpts = poly.data[0::2]
+        ypts = poly.data[1::2]
+
+        assert poly.layer == path.layer
+        assert xpts == pytest.approx(expected_xpts)
+        assert ypts == pytest.approx(expected_ypts)
+
